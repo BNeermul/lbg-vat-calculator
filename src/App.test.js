495c1667c839491744583e4cf14dac343a90205d
bryan <bryanneermul@gmail.com>
2023-10-31 09:32:04 +0000

    comments added and sonarqube test

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,7 @@ import App from './App';
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // hello
+// hello 2
 // });
 
 test('renders VAT Calculator text', () => {
